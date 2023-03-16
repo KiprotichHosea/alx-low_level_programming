66de625acc6d22a64f0600444d6dc9f0844fe8d1
KiprotichHosea <hoseahcollins@gmail.com>
2023-03-16 07:16:11 -0700

    Added a script that prints a line using puts function

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/**
+ * main - A programme that prints a line with puts function.
+ * return: o (success)
+ */
+int main(void){
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
