71aadf1fe46628167d5048946a2ebd3eeffcd92f
KiprotichHosea <hoseahcollins@gmail.com>
2023-03-23 07:21:43 -0700

    Task 2

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+ * mul - prints multiple of two intergers
+ *
+ * @a: parameter a
+ * @b: parameter b
+ *
+ * Return: (a * b)
+ */
+
+int mul(int a, int b)
+{
+	return (a * b);
+}
