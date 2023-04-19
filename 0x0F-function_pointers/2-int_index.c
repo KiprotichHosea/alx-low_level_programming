5b8e40372052b422684bd5deb772dfb83b2ef928
KiprotichHosea <hoseahcollins@gmail.com>
2023-04-19 08:18:16 -0700

    Task 2

---
+++ 0x0F-function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include "function_pointers.h"
+
+/**
+ * int_index - returns the index of first element
+ * @array: a pointer to an integer array
+ * @size: number of elements in the array
+ * @cmp: pointer to a function
+ * Return: 0
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+	{
+		return (-1);
+	}
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+	return (-1);
+}
