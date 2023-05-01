f6d12402f6a93fffad8ad0c59b852f5da5151dc8
KiprotichHosea <hoseahcollins@gmail.com>
2023-05-01 05:34:55 -0700

    Task 1->returns the number of elements in listint_t

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -2,12 +2,12 @@
 #include "lists.h"
 
 /**
- * list_len - returns the number of elements in a linked list
+ * listint_len - returns the number of elements in a linked list
  * @h: pointer to the list_t list
  *
  * Return: number of elements in h
  */
-size_t list_len(const list_t *h)
+size_t listint_len(const listint_t *h)
 {
 	size_t t = 0;
 
